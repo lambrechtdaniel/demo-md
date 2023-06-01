015260144ee06066f845dd67f9f91a881edf881c
Daniel Lambrecht <dl@neckar.it>
2023-06-01 11:15:28 +0200

    updated readme

--- main.js
+++
@@ -1,7 +0,0 @@
-import './style.css';
-import * as meisterCharts from '@meistercharts/meistercharts';
-
-let chart = meisterCharts.createTimeLineChartFromId('timeLineChart');
-
-// create the first sample data
-chart.setUpDemo();
